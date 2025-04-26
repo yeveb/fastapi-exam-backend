81c25dec4ef7e8d764024c060824565c2d61634c
yeveb <benkliy@gmail.com>
2025-04-26 19:09:38 +0300

    Supabase Key düzeltildi

--- supabase_submitter.py
+++ supabase_submitter.py
@@ -3,8 +3,8 @@ import requests
 from datetime import datetime
 
 # Burası kendi Supabase API bilgilerinle değiştirilecek
-SUPABASE_URL = "https://pvunkmlhgnqjryveftie.supabase.co"
-SUPABASE_KEY = "SeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2dW5rbWxoZ25xanJ5dmVmdGllIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTM5MzYyMiwiZXhwIjoyMDYwOTY5NjIyfQ.FaB-Y0nyvAPsyAuasOw3n2I2yiQE5OKdFKqMArQuqBw"  # Bunu kendi keyinle değiştir.
+SUPABASE_URL = "https://pvunkmlhgnqjryveftie.supabase.co/rest/v1/exam_responses"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2dW5rbWxoZ25xanJ5dmVmdGllIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NTM5MzYyMiwiZXhwIjoyMDYwOTY5NjIyfQ.FaB-Y0nyvAPsyAuasOw3n2I2yiQE5OKdFKqMArQuqBw"  # Bunu kendi keyinle değiştir.
 
 def submit_exam_response(form_data: dict) -> bool:
     headers = {
